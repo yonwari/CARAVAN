020380b66678d06fde36d04ab966fa73fd62e815
松本望実 <nozomi@nozomi.local>
2019-02-05 16:39:03 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,62 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blops_path
   end
 
   def edit
   end
+
+  def update
+  end
+
+# 以下プライベートアクション
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
